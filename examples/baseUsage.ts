27b42b6eed8f681f1191f3176913583a813d9cc9
tungnt1203 <tungnguyen120301@gmail.com>
2024-10-09 17:08:19 +0700

    crud mind

--- examples/baseUsage.ts
+++ examples/baseUsage.ts
@@ -1,10 +1,25 @@
 import Client from '../src/client';
-const client = new Client("")
+import { DatabaseConfig } from '../src/types/databaseConfig';
+const client = new Client('');
 
 async function getDatasources() {
+  const postgres_config = {
+    name: 'my_datasource',
+    description: '<baab',
+    engine: 'postgres',
+    connection_data: {
+      user: 'demo_user',
+      password: 'demo_password',
+      host: 'samples.mindsdb.com',
+      port: 5432,
+      database: 'demo',
+      schema: 'demo_data',
+    },
+  };
+
   try {
-    const data = await client.datasources.list();
-    return data
+    const mind = await client.minds.get('test_mind');
+    mind.completion("hãy cho tôi biết số lượng bảng ghi lớn nhất", true)
   } catch (error) {
     console.error(error);
   }
--- src/minds.ts
+++ src/minds.ts
@@ -1,12 +1,14 @@
 import { Datasource, DatabaseConfig } from './types/databaseConfig';
-import { MindInterface } from './types/mindInterface';
+const DEFAULT_PROMPT_TEMPLATE = "Use your database tools to answer the user's question: {{question}}";
+import OpenAI from 'openai';
+import { Stream } from 'openai/streaming';
 interface MindParameters {
   prompt_template?: string;
   [key: string]: any;
 }
-class Mind implements MindInterface {
-  api: any;
-  client: any;
+class Mind {
+  public api: any;
+  public client: any;
   project: string;
   name: string;
   model_name: string | null;
@@ -94,7 +96,25 @@ class Mind implements MindInterface {
     const updated = await this.client.minds.get(this.name);
     this.datasources = updated.datasources;
   }
+
+  async completion(message: string, stream: boolean = false) {
+    const openai_client = new OpenAI({ apiKey: this.client.apiKey, baseURL: this.client.baseUrl });
+    const response = await openai_client.chat.completions.create({
+      model: this.name,
+      messages: [{ role: 'user', content: message }],
+      stream: stream,
+    });
+
+    if (response instanceof Stream) {
+      for await (const chunk of response) {
+        process.stdout.write(chunk.choices[0]?.delta?.content || '');
+      }
+    } else {
+      return response.choices[0]?.message?.content || '';
+    }
+  }
 }
+
 class Minds {
   public client: any;
   public api: any;
@@ -108,54 +128,118 @@ class Minds {
 
   async list(): Promise<Mind[]> {
     const res = await this.api.get(`/api/projects/${this.project}/minds`);
-    return res.data.map(
-      (item: any) =>
+    const minds = res.data.map(
+      (mind: any) =>
         new Mind(
           this.client,
-          item.name,
-          item.model_name,
-          item.provider,
-          item.parameters,
-          item.datasources,
-          item.created_at,
-          item.updated_at,
+          mind.name,
+          mind.model_name,
+          mind.provider,
+          mind.parameters,
+          mind.datasources,
+          mind.created_at,
+          mind.updated_at,
         ),
     );
+    return minds;
+  }
+
+  async create(
+    mind_name: string,
+    options: {
+      model_name?: string;
+      provider?: string;
+      prompt_template?: string;
+      datasources?: (Datasource | string | DatabaseConfig)[];
+      parameters?: Record<string, any>;
+      replace?: boolean;
+    } = {},
+  ): Promise<Mind> {
+    let { model_name, provider, prompt_template, datasources = [], parameters = {}, replace = false } = options;
+
+    if (replace) {
+      try {
+        this.get(mind_name);
+        this.drop(mind_name);
+      } catch (err) {
+        console.log("Mind doesn't exist, creating new one");
+      }
+    }
+
+    const ds_names: string[] = [];
+    for (const datasource of datasources) {
+      const ds = this._check_datasource(datasource);
+      ds_names.push(ds);
+    }
+
+    if (prompt_template) {
+      parameters['prompt_template'] = prompt_template;
+    }
+    if (!('prompt_template' in parameters)) {
+      parameters['prompt_template'] = DEFAULT_PROMPT_TEMPLATE;
+    }
+
+    await this.api.post(`/api/projects/${this.project}/minds`, {
+      name: mind_name,
+      model_name: model_name,
+      provider: provider,
+      parameters: parameters,
+      datasources: ds_names,
+    });
+
+    const mind = this.get(mind_name);
+    return mind;
   }
 
   async get(name: string): Promise<Mind> {
     const resp = await this.api.get(`api/projects/${this.project}/minds/${name}`);
-    const item = resp.data;
+    const mind = resp.data;
     return new Mind(
       this.client,
-      item.name,
-      item.model_name,
-      item.provider,
-      item.parameters,
-      item.datasources,
-      item.created_at,
-      item.updated_at,
+      mind.name,
+      mind.model_name,
+      mind.provider,
+      mind.parameters,
+      mind.datasources,
+      mind.created_at,
+      mind.updated_at,
     );
   }
 
-  // check_datasource(ds: Datasource | DatabaseConfig | string): string {
-  //   if (typeof ds === 'string') {
-  //     return ds;
-  //   } else if ('name' in ds) {
-  //     return ds.name;
-  //   } else {
-  //     try {
-  //       this.client.datasources.get(ds.name);
-  //     } catch (error) {
-  //       if (error.name === 'ObjectNotFound') {
-  //         this.client.datasources.create(ds);
-  //       } else {
-  //         throw error;
-  //       }
-  //     }
-  //     return ds.name;
-  //   }
-  // }
+  async drop(mind_name: string): Promise<void> {
+    const resp = await this.api.delete(`/api/projects/mindsdb/minds/${mind_name}`);
+    return resp.data;
+  }
+
+  _check_datasource(ds: Datasource | DatabaseConfig | string): string {
+    if (this.isDatasource(ds)) {
+      return ds.name;
+    } else if (this.isDatabaseConfig(ds)) {
+      // if not exists - create
+      try {
+        this.client.datasources.get(ds.name);
+      } catch (error) {
+        if (error instanceof Error && error.name === 'ObjectNotFound') {
+          this.client.datasources.create(ds);
+        } else {
+          throw error;
+        }
+      }
+      return ds.name;
+    } else if (typeof ds === 'string') {
+      return ds;
+    } else {
+      throw new Error(`Unknown type of datasource: ${ds}`);
+    }
+  }
+
+  private isDatasource(ds: any): ds is Datasource {
+    return (ds as Datasource).name !== undefined;
+  }
+
+  private isDatabaseConfig(ds: any): ds is DatabaseConfig {
+    return (ds as DatabaseConfig).name !== undefined && (ds as DatabaseConfig).engine !== undefined;
+  }
 }
 
 export { Minds };
